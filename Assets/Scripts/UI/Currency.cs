b19d126ee253a448273f89f412e64215782fb590
FedericoJVillarreal <103009712+FedericoJVillarreal@users.noreply.github.com>
2023-06-11 23:45:29 -0300

    lol

--- Assets/Scripts/UI/Currency.cs
+++ Assets/Scripts/UI/Currency.cs
@@ -2,21 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Currency : MonoBehaviour
 {
-    public int currency;
-    public Text currencyText;
+    public int currency = 1000;
+    //public Text currencyText;
+
+    public TMP_Text goldCurrency;
+    //public TMP_Text energyCurrency;
+
+
+    private void Update()
+    {
+        
+        goldCurrency.text = currency.ToString();
+    }
+
 
     //void Start()
     //{
     //    currencyText.text = currency.ToString();
     //}
 
-
     public void LvlComplete()
     {
         currency = currency + 100;
+        Debug.Log("+100");
+
+
     }
 
 
--- Assets/Scripts/UI/ShopController.cs
+++ Assets/Scripts/UI/ShopController.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] ShopItem[] myItems;
+
+   // [SerializeField] ShopButton buttonPrefab = null;
+    
     void Start()
     {
-        
+        for(int i = 0; i >= myItems.Length; i++)
+        {
+
+        }
     }
 
     // Update is called once per frame
